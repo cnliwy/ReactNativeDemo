57489f1b3c76efc37988b1e097b3a779a28c63fa
liwy <cnliwy@163.com>
2017-11-27 22:35:03 +0800

    Initial commit

---
+++ app/src/main/java/com/liwy/reactnativedemo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.liwy.reactnativedemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.react.LifecycleState;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.shell.MainReactPackage;
+
+public class MainActivity extends AppCompatActivity {
+    private ReactRootView mReactRootView;
+    private ReactInstanceManager mReactInstanceManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mReactRootView = new ReactRootView(this);
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModuleName("index.android")
+                .addPackage(new MainReactPackage())
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build();
+        mReactRootView.startReactApplication(mReactInstanceManager, "NativeWorld", null);
+        setContentView(mReactRootView);
+//        setContentView(R.layout.activity_main);
+    }
+}
