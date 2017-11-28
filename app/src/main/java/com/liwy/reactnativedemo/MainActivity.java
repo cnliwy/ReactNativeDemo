4d97d470339bd65d59a6b416923191a57670bd76
cnliwy <cnliwy@163.com>
2017-11-28 12:49:06 +0800

    调试

--- app/src/main/java/com/liwy/reactnativedemo/MainActivity.java
+++ app/src/main/java/com/liwy/reactnativedemo/MainActivity.java
@@ -1,31 +1,108 @@
 package com.liwy.reactnativedemo;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 
-import com.facebook.react.LifecycleState;
+import com.facebook.react.BuildConfig;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
+import com.facebook.react.common.LifecycleState;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.facebook.react.shell.MainReactPackage;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DefaultHardwareBackBtnHandler {
+
     private ReactRootView mReactRootView;
     private ReactInstanceManager mReactInstanceManager;
+    private int OVERRLAY_CODE = 106;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            if(!Settings.canDrawOverlays(this)){
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:"+getPackageName()));
+                startActivityForResult(intent,OVERRLAY_CODE);
+            }
+        }
         mReactRootView = new ReactRootView(this);
         mReactInstanceManager = ReactInstanceManager.builder()
                 .setApplication(getApplication())
                 .setBundleAssetName("index.android.bundle")
-                .setJSMainModuleName("index.android")
+                .setJSMainModulePath("index.android")
+//                .setJSMainModuleName("index.android")
                 .addPackage(new MainReactPackage())
-                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setUseDeveloperSupport(true)
                 .setInitialLifecycleState(LifecycleState.RESUMED)
                 .build();
         mReactRootView.startReactApplication(mReactInstanceManager, "NativeWorld", null);
         setContentView(mReactRootView);
-//        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==OVERRLAY_CODE){
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                if(!Settings.canDrawOverlays(this)){
+                    Log.d("reactnativedemo", "onActivityResult: ");
+                }
+            }
+        }
+    }
+    @Override
+    public void invokeDefaultOnBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostPause(this);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostResume(this,this);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostDestroy(this);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onBackPressed();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
+            mReactInstanceManager.showDevOptionsDialog();
+            return true;
+        }
+        return super.onKeyUp(keyCode, event);
     }
 }
---
+++ app/src/main/java/com/liwy/reactnativedemo/MyApplication.java
@@ -0,0 +1,15 @@
+package com.liwy.reactnativedemo;
+
+import android.content.Context;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+
+/**
+ * Created by liwy on 2017/11/28.
+ */
+
+public class MyApplication extends  ReactApplicationContext {
+    public MyApplication(Context context) {
+        super(context);
+    }
+}
